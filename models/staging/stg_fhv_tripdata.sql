213a5371b2e67ce3094ebd907bc10d9fa35a220d
Aida Rahim <otheraida@gmail.com>
2022-02-19 04:15:02 +0000

    created stg_fhv_tripdata.sql

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='view') }}
+
+-- de-duplicate the data
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','external_table2') }}
+  where dispatching_base_num is not null 
+)
+
+select 
+-- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as string) as dispatching_base_num,
+    cast(PULocationID as integer) as PULocationID,
+    cast(DOLocationID as integer) as  DOLocationID,
+    cast(SR_Flag as string) as SR_Flag,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+from tripdata
+-- where rn = 1
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+-- {% if var('is_test_run', default=true) %}
+
+--   limit 100
+
+-- {% endif %}
